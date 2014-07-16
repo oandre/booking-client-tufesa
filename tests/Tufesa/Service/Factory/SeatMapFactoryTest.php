e97495e2dfa1cf05c767bdfb5ed3bc88c19c2853
Marcelo Santos <marcelsud@gmail.com>
2014-07-16 13:42:31 -0500

    Added the phpunit configuration and pointed to the bootstrap.php

--- tests/Tufesa/Service/Factory/SeatMapFactoryTest.php
+++ tests/Tufesa/Service/Factory/SeatMapFactoryTest.php
@@ -2,7 +2,15 @@
 
 namespace Tufesa\Service\Factory;
 
-class CategoryFactoryTest extends \PHPUnit_Framework_TestCase
+class SeatMapFactoryTest extends \PHPUnit_Framework_TestCase
 {
+    public function test_create_seat_with_valid_data_should_return_a_valid_instance_of_seat()
+    {
+        $seat = [
+            "_id" => 1,
+            "_available" => true
+        ];
 
+        $this->assertInstanceOf('Tufesa\Service\Type\Seat', SeatMapFactory::createSeat($seat));
+    }
 }
